3fa4f8797c83e3a54fb7308b8ec9c151de7150c0
kiomnd2 <kiomnd2@naver.com>
2024-04-19 23:20:27 +0900

    git first commit 최초 readme 작성

---
+++ src/main/java/com/example/nearbyfriend/NearbyFriendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nearbyfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyFriendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NearbyFriendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nearbyfriend/NearbyFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nearbyfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearbyFriendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
